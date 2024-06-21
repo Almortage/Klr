5b79b5430f9f9881bc4e1842f5eb95f423884277
Moalmortagel <105941198+Almortage@users.noreply.github.com>
2024-06-21 16:17:58 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -7,7 +7,7 @@ from pyrogram import filters
 load_dotenv()
 
 OWNER = ["Almortagel_12"]
-OWNER_NAME = "? ? ? ?? ?????????? ???????"
+OWNER_NAME = "ᯓ 𓆩 ˹ ᴅᴠ ᴇʟᴍᴏʀᴛᴀɢᴇʟ ˼⍣⃝🇪🇬𓆪𓆃"
 BOT_TOKEN = getenv("BOT_TOKEN")
 DATABASE = getenv("MONGO_DB_URI", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
@@ -16,4 +16,4 @@ CHANNEL = "https://t.me/AlmortagelTech"
 GROUP = "https://t.me/AlmortagelTech2"
 VID_SO = "https://telegra.ph/file/bf1273e084a0fb135ab5a.jpg"
 PHOTO = "https://telegra.ph/file/bf1273e084a0fb135ab5a.jpg"
-LOGS = "jabababbab"
+LOGS = "masdry7"
