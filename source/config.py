1139515f765af0c323057fe6208e471e5a3a7430
Moalmortagel <105941198+Almortage@users.noreply.github.com>
2024-06-27 02:42:33 +0300

    Update config.py

--- source/config.py
+++ source/config.py
@@ -1,24 +1,21 @@
-from almortagel import Almortagel
 from pyrogram import Client,filters,enums
 import redis
-
 r = redis.Redis(
-  host='redis-17811.c323.us-east-1-2.ec2.cloud.redislabs.com',
-  port=17811,
-  password='y41sFD7N8cY5Ob2MGPZkGdrTndVFY92h')
+    host="127.0.0.1",
+    port=6379,
+    charset="utf-8",
+    decode_responses=True
+    )
 
 sudo_id = 5089553588
-bot_user = Almortagel.BOT_USER
-via_user = Almortagel.VIA_USER
-elhyba = bot_user
-via = via_user
-api_id = Almortagel.APP_ID
-api_hash = Almortagel.API_HASH
-session = Almortagel.SESSION
-token = Almortagel.TG_BOT_TOKEN
+bot_user = "Almortagel1bot"
+api_id = 10823881
+api_hash = "339886e2109eb67203ce12022b32e035"
+session = "BAClb5LHg5JEvqA3mSo3UU-w9jZSrqrV9R7PdTnQ6F-3unwGhHETe2hq93gWqdqErRUn7F1Y-i9EXNPvXT0zzE-oRv6GjgBg_XmH6JfMTVBZktlravBc2yaUnMSlD0w96aEMVd029IWdJHRE73gtCNzQvlzk3Mf6X9DqGeNZbKF4gp_4rxtnpkZpTn1g9XAiU_ZSQmknFXFjiaQ7oISKqaPPN6kXvleQnIj1qFUHrw6j7GDvW7hHlh1-vjnBYe_uhGkbJpWEOkBBZ99re5f0YjmN3CDpgQYKLWhwomhPtiGdIUGIgxZ90Nfu8pD7bzXlwH-i8_NjW6XLquGNVhLsh4-NAAAAAXJdAbYA"
+token = "6247911571:AAE5BigKpsEb26VLCi19M3V-nWg_L5NQY4Y"
 sudo_command = [5089553588]
-pm =  Almortagel.MENTION
-mention = "5089553588"
+pm = "-100"
+mention = "-100"
 plugins = dict(root="plugins")
-app = Client(via,api_id , api_hash ,in_memory=True,session_string = session,plugins=plugins)
-bot = Client(elhyba,api_id=api_id , api_hash=api_hash ,bot_token=token,plugins=dict(root="plug_bot"))
+app = Client("user:Almortagel1bot",api_id , api_hash ,in_memory=True,session_string = session,plugins=plugins)
+bot = Client("Almortagel1bot",api_id=api_id , api_hash=api_hash ,bot_token=token,plugins=dict(root="plug_bot"))
